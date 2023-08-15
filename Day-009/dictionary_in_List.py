e97330fc663ec39ba30a7d16a7048bd7a7fcf1d4
Praneeth <kommusaipraneeth@gmail.com>
2023-08-15 20:05:25 +0530

    Day-9 Completed

---
+++ Day-009/dictionary_in_List.py
@@ -0,0 +1,21 @@
+travel_log = [
+    {
+        'country': 'USA',
+        'visits': 1,
+        'cities': ['Newark, Jersey City, New York']
+    },
+    {
+        'country': 'India',
+        'visits': 7,
+        'cities': ['Chennai, Delhi, Kerala, Andhra Pradesh']
+    }
+]
+
+
+def add_new_countries(country, visits, cities):
+    new_country = {'country': country, 'visits': visits, 'cities': cities}
+    travel_log.append(new_country)
+
+
+add_new_countries('UK', 1, ['london', 'Liverpool', 'Manchester'])
+print(travel_log)
