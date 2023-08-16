8b9a517af99ce064024aae49f3a594246e749c30
Praneeth <kommusaipraneeth@gmail.com>
2023-08-16 23:00:15 +0530

    Day-11 Completed

--- Day-011/BlackJack Capstone Project/main.py
+++ Day-011/BlackJack Capstone Project/main.py
@@ -98,6 +98,5 @@ def play_game():
 
 
 # Ask the user if they want to restart the game.
-# If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
     play_game()
