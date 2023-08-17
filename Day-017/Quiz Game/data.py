8200e1f105e9a7101b01f8d7e40d207eda8ae56e
Praneeth <kommusaipraneeth@gmail.com>
2023-08-17 23:32:45 +0530

    added questions to question_bank

---
+++ Day-017/Quiz Game/data.py
@@ -0,0 +1,18 @@
+question_data = [
+    {"text": "RAM stands for Random Access Memory.", "answer": "True"},
+    {"text": "A byte consists of 4 bits.", "answer": "False"},
+    {"text": "HTML is a programming language.", "answer": "False"},
+    {"text": "A firewall protects a computer network from unauthorized access.", "answer": "True"},
+    {"text": "Binary code is the representation of data using a combination of 0s and 2s.", "answer": "False"},
+    {"text": "Linux is an open-source operating system.", "answer": "True"},
+    {"text": "HTTP stands for Hypertext Transfer Protocol "
+             "and is used for secure data transmission over the internet.", "answer": "False"},
+    {"text": "The 'Ctrl + Z' keyboard shortcut is used to "
+             "redo the last action in most applications.", "answer": "False"},
+    {"text": "An algorithm is a step-by-step procedure for solving a problem.", "answer": "True"},
+    {"text": "The motherboard is the main circuit board of a computer where components "
+             "like CPU, RAM, and storage are connected.", "answer": "True"},
+    {"text": "JavaScript is a statically typed programming language.", "answer": "False"},
+    {"text": "Virtual reality (VR) technology can only be experienced "
+             "through specialized VR headsets.", "answer": "False"},
+]
