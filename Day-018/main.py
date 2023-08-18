d8bd48672790ad67e89e6e6c600fdc3641fb0a33
Praneeth <kommusaipraneeth@gmail.com>
2023-08-18 23:46:45 +0530

    draw spirograph

--- Day-018/main.py
+++ Day-018/main.py
@@ -24,6 +24,8 @@ tim.shape('turtle')
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180, 270]
+
+
 #
 #
 # def draw_shape(num_sides):
@@ -49,21 +51,33 @@ directions = [0, 90, 180, 270]
 
 # Random RGB color
 colormode(255)
-
-
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+    color = (r, g, b)
+    return color
 
 
-tim.pensize(10)
-for _ in range(50):
-    tim.forward(30)
-    tim.color(random_color())
-    tim.setheading(random.choice(directions))
+# tim.pensize(10)
+# for _ in range(50):
+#     tim.forward(30)
+#     tim.color(random_color())
+#     tim.setheading(random.choice(directions))
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+tim.speed("fastest")
+draw_spirograph(7)
+
+# draw a spirograph
+
 
 screen = Screen()
 screen.exitonclick()
