536dd4f2de0a3d241028ed9f558721cada83eb62
Praneeth <kommusaipraneeth@gmail.com>
2023-08-18 21:31:37 +0530

    Draw a square

--- Day-018/main.py
+++ Day-018/main.py
@@ -1,18 +1,16 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('turtle')
-timmy_the_turtle.color('red')
+tim = Turtle()
+tim.shape('turtle')
+tim.color('red')
+#
+# tim.forward(100)
+# tim.right(90)
 
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-
-
-
-
-
-
-
+# Draw a square
+for i in range(4):
+    tim.forward(100)
+    tim.right(90)
 
 screen = Screen()
 screen.exitonclick()
