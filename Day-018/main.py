9cdf5556b9bb21447b955cbe167e20b813b7d8f4
Praneeth <kommusaipraneeth@gmail.com>
2023-08-18 22:42:38 +0530

    draw different shapes and colors

--- Day-018/main.py
+++ Day-018/main.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle, Screen
 
 tim = Turtle()
@@ -13,13 +14,27 @@ tim.shape('turtle')
 #     tim.right(90)
 
 # Draw a dashed line
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+# Different Shapes
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
+          "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
 
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
