054e288f1188b26f8b6ceab8f50cd9a3f553fc7b
Praneeth <kommusaipraneeth@gmail.com>
2023-08-18 21:48:32 +0530

    Draw a dashed line

--- Day-018/main.py
+++ Day-018/main.py
@@ -2,15 +2,24 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 tim.shape('turtle')
-tim.color('red')
+# tim.color('red')
 #
 # tim.forward(100)
 # tim.right(90)
 
 # Draw a square
-for i in range(4):
-    tim.forward(100)
-    tim.right(90)
+# for i in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+
+# Draw a dashed line
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+
 
 screen = Screen()
 screen.exitonclick()
