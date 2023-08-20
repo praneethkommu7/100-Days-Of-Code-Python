5be8aca0fb469252e439902fbfd1d855cd5914a0
Praneeth <kommusaipraneeth@gmail.com>
2023-08-20 23:38:57 +0530

    GAMEOVER on collision with wall

--- Day-020 and 021/Snake game/scoreboard.py
+++ Day-020 and 021/Snake game/scoreboard.py
@@ -21,3 +21,7 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def gameover(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
