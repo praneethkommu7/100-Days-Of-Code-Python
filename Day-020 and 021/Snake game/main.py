93ef4649c31c5bff14a5d85b9f49f90e59e89760
Praneeth <kommusaipraneeth@gmail.com>
2023-08-20 23:56:04 +0530

    Day 20 & 21 Completed

--- Day-020 and 021/Snake game/main.py
+++ Day-020 and 021/Snake game/main.py
@@ -34,15 +34,13 @@ while game_is_on:
         scoreboard.increase_score()
 
     # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
 
     # Detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
         # if head collides with any segment in the tail:
