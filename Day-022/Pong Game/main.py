631dddd01993368ce47e0ff4596b44380c885d39
Praneeth <kommusaipraneeth@gmail.com>
2023-09-07 20:30:29 -0400

    ball Class and bounce method added

--- Day-022/Pong Game/main.py
+++ Day-022/Pong Game/main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("Black")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,6 +21,17 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # need to bounce
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.xcor() < -320 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
 
 screen.exitonclick()
