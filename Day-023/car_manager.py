c8b496be9cf2ebffb6d2a42a19609b954155d4b7
Praneeth <kommusaipraneeth@gmail.com>
2023-09-10 18:49:54 -0400

    Detect when the player has reached the other side

--- Day-023/car_manager.py
+++ Day-023/car_manager.py
@@ -1,5 +1,6 @@
 import random
 from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
@@ -8,6 +9,7 @@ MOVE_INCREMENT = 10
 class CarManager:
     def __init__(self):
         self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
         random_chance = random.randint(1, 6)
@@ -22,4 +24,7 @@ class CarManager:
 
     def move_cars(self):
         for car in self.all_cars:
-            car.backward(STARTING_MOVE_DISTANCE)
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
